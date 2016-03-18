8e4190913ab2b2a910881d98f7d522fd242dbe77
mladenlukic25714 <maxoandroid@gmail.com>
2016-03-18 18:15:31 +0100

    Dodata klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, double prodajni, double kupovni, double srednji) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datum, double prodajni, double kupovni, double srednji) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pronadjiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
